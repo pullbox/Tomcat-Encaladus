92fa6e19e21c77034736869e03e878a7f4b6af3a
pullb0x <daniel@bechtelus.net>
2021-09-08 13:37:35 -0400

    fixed

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t tomcat-enceladus:1.0 .
-docker build -t tomcat-enceladus:latest .
+docker build -t pullb0x/tomcat-enceladus:1.0 .
+docker build -t pullb0x/tomcat-enceladus:latest .
 docker push pullb0x/tomcat-enceladus:1.0
 docker push pullb0x/tomcat-enceladus:latest
