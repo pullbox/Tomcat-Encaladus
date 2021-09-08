282528f2e4e07c803c3196405690a6a73e72035d
pullb0x <daniel@bechtelus.net>
2021-09-08 13:36:17 -0400

    push to docker hub

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build -t tomcat-encaladus:1.0 .
+docker build -t tomcat-enceladus:1.0 .
+docker build -t tomcat-enceladus:latest .
+docker push pullb0x/tomcat-enceladus:1.0
+docker push pullb0x/tomcat-enceladus:latest
